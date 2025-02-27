d68ea18b260cc28c6490d4f0fec3fc3ad24880f0
Raman Kurai <ramankurai27@gmail.com>
2025-02-27 23:33:17 +0530

    add : skeleton routes for user

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,35 @@
-"First commit"
+const express = require("express")
+
+const app = express();
+
+app.post("/user/signup" , (req , res) => {
+    res.json({
+        message : "signup endpoint"
+    })
+})
+
+app.post("/user/signin" , (req , res)=>{
+     res.json({
+        message : "signin endpoint"
+     })
+})
+
+app.get("user/purchases" , (req, res)=>{
+   res.json({
+    message : ""
+   })
+})
+
+app.post("/course/purchase" , (req , res)=>{
+   res.json({
+    message : ""
+   })
+})
+
+app.get("courses" , (req ,res)=>{
+   res.json({
+    message : ""
+   })
+})
+
+app.listen(3000)
