58c09395e6d9adfb55eea5bc4abd24b946693f7e
Raman Kurai <ramankurai27@gmail.com>
2025-03-03 00:10:44 +0530

    add : JWT SECRET for user and admin

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 const MONGO_URL = process.env.MONGO_URL;
+const JWT_USER_SECRET = process.env.JWT_USER_SECRET;
+const JWT_ADMIN_SECRET = process.env.JWT_ADMIN_SECRET
 
 module.exports = {
-   MONGO_URL : MONGO_URL
-}
+  MONGO_URL: MONGO_URL,
+  JWT_USER_SECRET: JWT_USER_SECRET,
+  JWT_ADMIN_SECRET : JWT_ADMIN_SECRET
+};
