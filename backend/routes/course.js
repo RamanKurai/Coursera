5e30194ac5ba4c66737168b84581afff558c59da
Raman Kurai <ramankurai27@gmail.com>
2025-03-07 23:55:10 +0530

    update : course routes endpoints and admin middleware

--- backend/routes/course.js
+++ backend/routes/course.js
@@ -1,17 +1,31 @@
-const {Router} = require("express")
+const {Router} = require("express");
+const { purchaseModel } = require("../db");
 
  const courseRouter = Router()
 
-courseRouter.post("/purchase" , (req , res)=>{
-    res.json({
-     message : ""
-    })
+courseRouter.post("/purchase" , async (req , res)=>{
+   const userId = req.userId;
+   const courseId = req.body.courseId;
+
+   // should check that the user has actually paid the price
+   await purchaseModel.create({
+       userId,
+       courseId
+   })
+
+   res.json({
+       message: "You have successfully bought the course"
+   })
  })
  
- courseRouter.get("/preview" , (req ,res)=>{
-    res.json({
-     message : ""
-    })
+ courseRouter.get("/preview" , async (req ,res)=>{
+   const courses = await courseModel.find({
+
+   });
+
+   res.json({
+       courses
+   })
  })
 
  module.exports = {
